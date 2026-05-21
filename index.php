7b70ce7e70d39544ac932aedce1804fb6856697a
Big_BossTaOtherLabOtherPC <isack_santana@estudante.sesisenai.org.br>
2026-05-21 09:30:34 -0300

    Added JS coding

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+session_start();
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sistema_simples";
+$conn = new mysqli($host,$user,$pass,$db)
+
+if ( $conn -> connect_error){
+    echo "<script>console.log('error na conexão com o banco');<script>";
+}else{
+    echo "<script>console.log('conexão com banco foi um sucesso');<script>";
+}
+
+?>
+
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +27,16 @@
 <body>
     
     <h1>Tela de Login PHP</h1>
+
+    <form method="POST">
+
+    <label>Usuario</label>
+    <input type="text" name="usuario"><br>
+    <label>Senha</label>
+    <input type="password"name="senha"><br>
+
+    <button type="submit">Entrar</button>
+    </form>
 
 </body>
 </html>
--- infra/db/script.sql
+++ infra/db/script.sql
@@ -3,13 +3,9 @@ CREATE DATABASE IF NOT EXISTS `sistema_simples`;
 USE `sistema_simples`;
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL
+);
 
-INSERT INTO `users` (`username`, `password`) VALUES
-('admin', 'admin123'),
-('user1', 'password1'),
-('user2', 'password2');
+INSERT INTO `users` (username, password) VALUES ('admin', 'admin123');
